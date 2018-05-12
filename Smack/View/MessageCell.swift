743b549d7d9f4161c514d5f4abadd725cb8313a3
egorchernenko <lordo916@gmail.com>
2018-05-12 12:14:00 +0300

    fix bug with date

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -25,5 +25,20 @@ class MessageCell: UITableViewCell {
         userNameLabel.text = message.userName
         userImageView.image = UIImage(named: message.userAvatar)
         userImageView.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 }
